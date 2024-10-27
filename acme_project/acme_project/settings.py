2ea8f75bd208b3def63a34822b4e2203a68f13ff
khalikov-sa <khalikov.ss@yandex.ru>
2024-10-27 06:49:33 +0300

    acme_9

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -32,6 +33,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'acme_project.urls'
 
 TEMPLATES_DIR = BASE_DIR / 'templates'
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 TEMPLATES = [
     {
